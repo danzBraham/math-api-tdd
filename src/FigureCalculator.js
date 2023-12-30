aef9381d02080fc7827fcaa030ebc634586c6b13
Zidan Abraham <zidangit07@gmail.com>
2023-12-30 16:03:20 +0800

    test(figurecalculator): add tests for calculateRectanglePerimeter function behavior

--- src/FigureCalculator.js
+++ src/FigureCalculator.js
@@ -1,9 +1,22 @@
+/* eslint-disable no-underscore-dangle */
 class FigureCalculator {
   constructor(mathBasic) {
-    this.mathBasic = mathBasic;
+    this._mathBasic = mathBasic;
   }
 
-  calculateRectanglePerimeter() {}
+  calculateRectanglePerimeter(...args) {
+    if (args.length !== 2) {
+      throw new Error('function only accepts two parameters');
+    }
+
+    const [length, width] = args;
+
+    if (typeof length !== 'number' || typeof width !== 'number') {
+      throw new Error('function only accepts the number parameter');
+    }
+
+    return this._mathBasic.multiply(2, this._mathBasic.add(length, width));
+  }
 
   calculateRectangleArea() {}
 
--- src/FigureCalculator.test.js
+++ src/FigureCalculator.test.js
@@ -1,4 +1,5 @@
 const FigureCalculator = require('./FigureCalculator');
+const MathBasic = require('./MathBasic');
 
 describe('A FigureCalculator', () => {
   it('should have calculateRectanglePerimeter, calculateRectangleArea, calculateTrianglePerimeter, and calculateTriangleArea functions', () => {
@@ -13,5 +14,41 @@ describe('A FigureCalculator', () => {
     expect(figureCalculator.calculateRectangleArea).toBeInstanceOf(Function);
     expect(figureCalculator.calculateTrianglePerimeter).toBeInstanceOf(Function);
     expect(figureCalculator.calculateTriangleArea).toBeInstanceOf(Function);
+  });
+
+  describe('A calculateRectanglePerimeter function', () => {
+    it('should throw error when not given 2 parameters', () => {
+      const figureCalculator = new FigureCalculator({});
+
+      expect(() => figureCalculator.calculateRectanglePerimeter()).toThrow();
+      expect(() => figureCalculator.calculateRectanglePerimeter(1)).toThrow();
+      expect(() => figureCalculator.calculateRectanglePerimeter(1, 2, 3)).toThrow();
+      expect(() => figureCalculator.calculateRectanglePerimeter(1, 2, 3, 4)).toThrow();
+    });
+
+    it('should throw error when given non-number parameters', () => {
+      const figureCalculator = new FigureCalculator({});
+
+      expect(() => figureCalculator.calculateRectanglePerimeter('1', '2')).toThrow();
+      expect(() => figureCalculator.calculateRectanglePerimeter(true, {})).toThrow();
+      expect(() => figureCalculator.calculateRectanglePerimeter(null, false)).toThrow();
+    });
+
+    it('should return the correct value based on the perimeter of a square formula', () => {
+      // Arrange
+      const length = 10;
+      const width = 20;
+      const spyAdd = jest.spyOn(MathBasic, 'add');
+      const spyMultiply = jest.spyOn(MathBasic, 'multiply');
+      const figureCalculator = new FigureCalculator(MathBasic);
+
+      // Action
+      const result = figureCalculator.calculateRectanglePerimeter(length, width);
+
+      // Assert
+      expect(result).toEqual(60);
+      expect(spyAdd).toHaveBeenCalledWith(length, width);
+      expect(spyMultiply).toHaveBeenCalledWith(2, 30);
+    });
   });
 });
